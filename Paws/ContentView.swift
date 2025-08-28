add14aa6204f1f321ec43711a24b8c01b3a1ca0a
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-08-28 14:25:29 +0800

    ContentView (using reusable component)

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -3,22 +3,31 @@
 //  Paws
 //
 //  Created by Jourdese Palacio on 8/28/25.
-//
+//  Content View
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query private var pets: [Pet]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            ScrollView {
+                EmptyView()
+            } //#Scrollview
+            .overlay {
+                if pets.isEmpty {
+                    CustomContentUnavailableView(
+                        icon: "dog.circle", title: "No Pets", description: "Add a new pet to get started"
+                    )
+                }
+            }
+        } //#NavigationStack
     }
 }
 
-#Preview {
+#Preview("No Data") {
     ContentView()
+        .modelContainer(for: Pet.self, inMemory: true)
 }
