c9f3f05c7b0b93f5561ce413a66b261ad58760c6
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-08-28 12:48:15 +0800

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Jourdese Palacio on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
